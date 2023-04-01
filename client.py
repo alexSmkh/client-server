4e51f1d55c500f7abc0b2ed6569d4f9b4d5a241f
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-04-01 17:59:13 +0300

    feature: add base chat implementation

---
+++ client.py
@@ -0,0 +1,72 @@
+import os
+import socket
+import threading
+
+from config import QUIT_EVENT, HOST, PORT
+
+
+def read_messages(client: socket.socket, is_running: threading.Event) -> None:
+    try:
+        while is_running.is_set():
+            message = client.recv(1024).decode()
+            print(message)
+    except (socket.error, ConnectionError, ConnectionResetError):
+        print('### Connection lost ###')
+
+
+def send_messages(client: socket.socket, is_running: threading.Event) -> None:
+    try:
+        nickname = input('### Choose your nickname: ')
+        client.send(nickname.encode())
+
+        while is_running.is_set():
+            message = input('')
+            client.send(message.encode())
+
+            if message == QUIT_EVENT:
+                is_running.clear()
+                break
+    except (socket.error, ConnectionError, ConnectionResetError):
+        print('### Connection lost ###')
+
+
+def main() -> None:
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((HOST, PORT))
+
+        is_running = threading.Event()
+        is_running.set()
+
+        read_msgs_thread = threading.Thread(
+            target=read_messages,
+            args=(client, is_running),
+            daemon=True,
+        )
+        send_msgs_thread = threading.Thread(
+            target=send_messages,
+            args=(client, is_running),
+            daemon=True,
+        )
+
+        read_msgs_thread.start()
+        send_msgs_thread.start()
+
+        read_msgs_thread.join()
+        send_msgs_thread.join()
+
+    except ConnectionRefusedError:
+        print('### Connection refused ###')
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print('### You have been disconnected ###')
+        if client is not None:
+            client.close()
+
+        if not is_running.is_set():
+            os._exit(0)
+
+
+if __name__ == '__main__':
+    main()
